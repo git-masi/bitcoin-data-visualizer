ce88d0135982a1cbc8252790ea53376223e9284c
Eric Masi <eric@gitmasi.com>
2019-05-23 10:13:28 -0400

    add PriceIndex component and route

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import PriceIndex from './components/PriceIndex';
 
 const Routes = props => {
   return (
@@ -8,7 +9,7 @@ const Routes = props => {
       <Route exact path="/" render={routeProps => <Home {...routeProps}/>}/>
       <Route exact path="/about" render={routeProps => <h1>this is the about page</h1>}/>
       <Route exact path="/invest" render={routeProps => <h1>this is the invest page</h1>}/>
-      <Route exact path="/price-index" render={routeProps => <h1>this is the price index page</h1>}/>
+      <Route exact path="/price-index" render={routeProps => <PriceIndex {...routeProps} />}/>
     </Switch>
   )
 }
---
+++ src/components/PriceIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+  
+class PriceIndex extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    )
+  }
+}
+
+export default PriceIndex;
