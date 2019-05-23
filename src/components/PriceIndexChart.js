996608219ce8474abd42d98b3f9095428309ff1f
Eric Masi <eric@gitmasi.com>
2019-05-23 16:27:42 -0400

    cleanup work

--- src/components/PriceIndexChart.js
+++ src/components/PriceIndexChart.js
@@ -10,7 +10,7 @@ class PriceIndexChart extends Component {
   }
   
   async getPriceIndexData() {
-    const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-05-01&end=2019-05-23');
+    const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-05-11&end=2019-05-23');
     let data = await response.json();
     return data;
   }
@@ -25,87 +25,54 @@ class PriceIndexChart extends Component {
   }
 
 
-getColorStopColors = () => {
-  const colors = ['rgba(231, 76, 60, 1)', 'rgba(241, 196, 15, 1)', 'rgba(46, 204, 113, 1)'];
-  const numArr = [...this.state.data];
-  const sortArr = [...numArr].sort((a,b) => a - b);
-  const breakPoint = Math.floor(numArr.length / 3);
-  const firstThird = sortArr[breakPoint];
-  const secondThird = sortArr[breakPoint * 2];
-  const max = Math.max(...numArr);
+  getColorStopColors = () => {
+    const colors = ['rgba(231, 76, 60, 1)', 'rgba(241, 196, 15, 1)', 'rgba(46, 204, 113, 1)'];
+    const numArr = [...this.state.data];
+    const sortArr = [...numArr].sort((a,b) => a - b);
+    const breakPoint = Math.floor(numArr.length / 3);
+    const firstThird = sortArr[breakPoint];
+    const secondThird = sortArr[breakPoint * 2];
+    const max = Math.max(...numArr);
 
-  let output = [];
-  
-  for (let n of numArr) {
-    switch (true) {
-      case n <= firstThird:
-        output.push(this.getRGBA(n, firstThird, colors[0]));
-        break;
-      case n <= secondThird:
-        output.push(this.getRGBA(n, secondThird, colors[1]));
-        break;
-      case n > secondThird:
-        output.push(this.getRGBA(n, max, colors[2]));
-        break;
-      default:
-        console.log('something went wrong');
-        break;
+    let output = [];
+    
+    for (let n of numArr) {
+      switch (true) {
+        case n <= firstThird:
+          output.push(this.getRGBA(n, firstThird, colors[0]));
+          break;
+        case n <= secondThird:
+          output.push(this.getRGBA(n, secondThird, colors[1]));
+          break;
+        case n > secondThird:
+          output.push(this.getRGBA(n, max, colors[2]));
+          break;
+        default:
+          console.log('something went wrong');
+          break;
+      }
     }
+    return output;
   }
 
-  return output;
-}
+  getRGBA = (num, max, clr) => {
+    return `rgba(${chroma(clr).alpha(num / max)._rgb.join(', ')})`;
+  }
 
-getRGBA = (num, max, clr) => {
-  return `rgba(${chroma(clr).alpha(num / max)._rgb.join(', ')})`;
-}
+  getColorStopStops = () => {
+    const length = this.state.data.length;
+    let output = [];
+    for (let i in this.state.data) {
+      output.push(i / length);
+    };
+    return output;
+  }
 
-getColorStopStops = () => {
-  const length = this.state.data.length;
-  let output = [];
-  for (let i in this.state.data) {
-    output.push(i / length);
-  };
-  return output;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // might be cool to add color stops that are color coded based on high and low points in the data 
   setColorGradient = (canvas) => {
-    const width = canvas.width;
-    const height = canvas.height;
+    const width = canvas.clientWidth;
     const ctx = canvas.getContext('2d');
-    // console.log(ctx);
-    const gradient = ctx.createLinearGradient(0, 0, width, height);
-    // gradient.addColorStop(0, 'rgba(200, 214, 229, .2)');
-    // gradient.addColorStop(.2, 'rgba(200, 214, 229, .4)');
-    // gradient.addColorStop(.5, 'rgba(254, 202, 87, .6)');
-    // gradient.addColorStop(1, 'rgba(254, 202, 87, 1)');
-    // this.getColorStops();
+    console.log(ctx);
+    const gradient = ctx.createLinearGradient(0, 0, width, 0);
     const grdntStops = this.getColorStopStops();
     const grdntColors = this.getColorStopColors();
     for (let i = 0; i < this.state.data.length; i++) {
