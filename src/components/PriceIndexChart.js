b8c4890efcfa2fe9b14a20eebb45523675f86cbd
Eric Masi <eric@gitmasi.com>
2019-05-23 13:57:54 -0400

    get bitcoin api data async, display in chart

--- src/components/PriceIndexChart.js
+++ src/components/PriceIndexChart.js
@@ -4,31 +4,48 @@ import { Line } from 'react-chartjs-2';
   
 class PriceIndexChart extends Component {
   state = {
-    data: {
-      labels: ['1', '2', '3', '4', '5'],
+    labels: [],
+    data: [],
+  }
+  
+  async getPriceIndexData() {
+    const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-05-17&end=2019-05-23');
+    let data = await response.json();
+    return data;
+  }
+
+  componentDidMount() {
+    this.getPriceIndexData()
+      .then(d => this.setState({
+        labels: Object.keys(d.bpi),
+        data: Object.values(d.bpi)     
+      }))
+      .catch(err => console.log(err))
+  }
+
+  makeChart = canvas => {
+    const chartData = {
+      labels: this.state.labels,
       datasets: [
         {
-          label: 'bitcoin mined',
+          label: 'bitcoin price',
           backgroundColor: 'rgba(238, 82, 83,1.0)',
-          data: [3,7,8,3,1,2,3,4],
+          data: this.state.data,
         },
-        {
-          label: 'bitcoin sold',
-          backgroundColor: 'rgba(16, 172, 132,1.0)',
-          data: [1,10,2,7,1,2,6,0],
-        }
       ]
     }
+    return chartData;
   }
 
   render() {
+
     return (
-      <div style={{position: 'relative', width: '100%'}}>
+      <div style={{position: 'relative'}}>
         <Line 
           options={{
             responsive: true,
           }}
-          data={this.state.data}
+          data={this.makeChart}
         />
       </div>
     )
