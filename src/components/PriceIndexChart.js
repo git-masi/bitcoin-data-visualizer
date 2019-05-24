c6aefa2bcb1cf896f9a0cd7af3f14ea53e5a7f45
Eric Masi <eric@gitmasi.com>
2019-05-24 12:50:30 -0400

    change gradient in chart

--- src/components/PriceIndexChart.js
+++ src/components/PriceIndexChart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 import Card from 'react-bootstrap/Card';
-import chroma from 'chroma-js';
+// import chroma from 'chroma-js';
 
   
 class PriceIndexChart extends Component {
@@ -18,7 +18,7 @@ class PriceIndexChart extends Component {
     return data;
   }
 
-  // not sure what this does but it gets ride of the error
+  // not sure what this does but it gets rid of the error
   // throws a DOMException though
   // I should ask about this
   controller = new AbortController();
@@ -36,60 +36,72 @@ class PriceIndexChart extends Component {
     this.controller.abort();
   }
 
-  getColorStopColors = () => {
-    const colors = ['rgba(231, 76, 60, 1)', 'rgba(241, 196, 15, 1)', 'rgba(46, 204, 113, 1)'];
-    const numArr = [...this.state.data];
-    const sortArr = [...numArr].sort((a,b) => a - b);
-    const breakPoint = Math.floor(numArr.length / 3);
-    const firstThird = sortArr[breakPoint];
-    const secondThird = sortArr[breakPoint * 2];
-    const max = Math.max(...numArr);
+  // getColorStopColors = () => {
+  //   const colors = ['rgba(231, 76, 60, 1)', 'rgba(241, 196, 15, 1)', 'rgba(46, 204, 113, 1)'];
+  //   const numArr = [...this.state.data];
+  //   const sortArr = [...numArr].sort((a,b) => a - b);
+  //   const breakPoint = Math.floor(numArr.length / 3);
+  //   const firstThird = sortArr[breakPoint];
+  //   const secondThird = sortArr[breakPoint * 2];
+  //   const max = Math.max(...numArr);
 
-    let output = [];
+  //   let output = [];
     
-    for (let n of numArr) {
-      switch (true) {
-        case n <= firstThird:
-          output.push(this.getRGBA(n, firstThird, colors[0]));
-          break;
-        case n <= secondThird:
-          output.push(this.getRGBA(n, secondThird, colors[1]));
-          break;
-        case n > secondThird:
-          output.push(this.getRGBA(n, max, colors[2]));
-          break;
-        default:
-          console.log('something went wrong');
-          break;
-      }
-    }
-    return output;
-  }
+  //   for (let n of numArr) {
+  //     switch (true) {
+  //       case n <= firstThird:
+  //         output.push(this.getRGBA(n, firstThird, colors[0]));
+  //         break;
+  //       case n <= secondThird:
+  //         output.push(this.getRGBA(n, secondThird, colors[1]));
+  //         break;
+  //       case n > secondThird:
+  //         output.push(this.getRGBA(n, max, colors[2]));
+  //         break;
+  //       default:
+  //         console.log('something went wrong');
+  //         break;
+  //     }
+  //   }
+  //   return output;
+  // }
 
-  getRGBA = (num, max, clr) => {
-    return `rgba(${chroma(clr).alpha(num / max)._rgb.join(', ')})`;
-  }
+  // getRGBA = (num, max, clr) => {
+  //   return `rgba(${chroma(clr).alpha(num / max)._rgb.join(', ')})`;
+  // }
 
-  getColorStopStops = () => {
-    const length = this.state.data.length;
-    let output = [];
-    for (let i in this.state.data) {
-      output.push(i / length);
-    };
-    return output;
-  }
+  // getColorStopStops = () => {
+  //   const length = this.state.data.length;
+  //   let output = [];
+  //   for (let i in this.state.data) {
+  //     output.push(i / length);
+  //   };
+  //   return output;
+  // }
 
   setColorGradient = (canvas) => {
-    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    // const width = canvas.clientWidth;
     const ctx = canvas.getContext('2d');
-    // console.log(ctx);
-    const gradient = ctx.createLinearGradient(0, 0, width, 0);
-    const grdntStops = this.getColorStopStops();
-    const grdntColors = this.getColorStopColors();
-    for (let i = 0; i < this.state.data.length; i++) {
-      gradient.addColorStop(grdntStops[i], grdntColors[i]);
-    }
-    // console.log(grdntStops, grdntColors);
+    console.log(ctx);
+    console.log(height);
+    console.log(ctx.canvas.clientHeight);
+
+    const gradient = ctx.createLinearGradient(0, height, 0, 0);
+    gradient.addColorStop(0, 'rgba(231, 76, 60, .7)');
+    gradient.addColorStop(.34, 'rgba(231, 76, 60, .7)');
+    gradient.addColorStop(.34, 'rgba(241, 196, 15, .7)');
+    gradient.addColorStop(.62, 'rgba(241, 196, 15, .7)');
+    gradient.addColorStop(.62, 'rgba(46, 204, 113, .7)');
+    gradient.addColorStop(1, 'rgba(46, 204, 113, .7)');
+
+    // const gradient = ctx.createLinearGradient(0, 0, width, 0);
+    // const grdntStops = this.getColorStopStops();
+    // const grdntColors = this.getColorStopColors();
+    // for (let i = 0; i < this.state.data.length; i++) {
+    //   gradient.addColorStop(grdntStops[i], grdntColors[i]);
+    // }
+
     return gradient;
   }
 
@@ -101,9 +113,8 @@ class PriceIndexChart extends Component {
           label: 'bitcoin price',
           data: this.state.data,
           backgroundColor: this.setColorGradient(canvas),
-          pointHoverBackgroundColor: 'rgba(254, 202, 87, 1)',
-          borderColor: 'rgba(10, 189, 227, .8)',
-          borderWidth: 5,
+          borderColor: 'rgba(52, 73, 94, 1.0)',
+          borderWidth: 2,
           borderJoinStyle: 'round',
           lineTension: .2,
         },
