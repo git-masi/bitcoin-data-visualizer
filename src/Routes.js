7865f81fd497811d9832f01a64699653ce1782ab
Eric Masi <eric@gitmasi.com>
2019-05-30 10:19:03 -0400

    create invest page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Invest from './components/Invest';
 import PriceIndex from './components/PriceIndex';
 
 const Routes = props => {
@@ -8,7 +9,7 @@ const Routes = props => {
     <Switch>
       <Route exact path="/" render={routeProps => <Home {...routeProps}/>}/>
       <Route exact path="/about" render={routeProps => <h1>this is the about page</h1>}/>
-      <Route exact path="/invest" render={routeProps => <h1>this is the invest page</h1>}/>
+      <Route exact path="/invest" render={routeProps => <Invest {...routeProps}/>}/>
       <Route exact path="/price-index" render={routeProps => <PriceIndex {...routeProps} />}/>
     </Switch>
   )
