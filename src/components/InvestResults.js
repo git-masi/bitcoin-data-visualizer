bc8b1683dcd84a329d1cbe4ce5c803c27b0a3cd9
Eric Masi <eric@gitmasi.com>
2019-05-31 11:20:38 -0400

    add doughnut chart

--- src/components/InvestResults.js
+++ src/components/InvestResults.js
@@ -2,16 +2,23 @@ import React, { Component, Fragment } from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
+import { Doughnut } from 'react-chartjs-2';
 import styles from './InvestResults.module.css';
   
 class InvestResults extends Component {
   render() {
     const { curPrice, salePrice, purchasePrice, saleQuant, purchaseQuant, ROI } = this.props;
-    const gainOrLoss = (salePrice * saleQuant) - (purchasePrice * purchaseQuant);
+
+    const principal = purchasePrice * purchaseQuant;
+    const totalSale = salePrice * saleQuant;
+    const gainOrLoss = (salePrice * saleQuant) - (purchasePrice * saleQuant);
+    const remValue = (purchaseQuant - saleQuant) * curPrice;
+
     const gainLossStyle = () => {
       if (gainOrLoss === 0) return '';
       return gainOrLoss > 0 ? styles.gain : styles.loss
     }
+
     return (
       <Fragment>
         <Col lg={4}>
@@ -22,11 +29,11 @@ class InvestResults extends Component {
                 <tbody>
                   <tr>
                     <td>Principal</td>
-                    <td>${(purchasePrice * purchaseQuant).toFixed(2)}</td>
+                    <td>${(principal).toFixed(2)}</td>
                   </tr>
                   <tr>
                     <td>Total Sale</td>
-                    <td>${(salePrice * saleQuant).toFixed(2)}</td>
+                    <td>${(totalSale).toFixed(2)}</td>
                   </tr>
                   <tr>
                     <td>Gain / Loss</td>
@@ -38,14 +45,35 @@ class InvestResults extends Component {
                   </tr>
                   <tr>
                     <td>Remaining Value</td>
-                    <td>${((purchaseQuant - saleQuant) * curPrice).toFixed(2)}</td>
+                    <td>${(remValue).toFixed(2)}</td>
                   </tr>
                 </tbody>
               </Table>
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={4}></Col>
+        <Col lg={4}>
+          <Card body className="mt-3">
+            <Doughnut
+              data = {{
+                datasets: [{
+                    data: [gainOrLoss, totalSale, remValue],
+                    backgroundColor: ['rgba(46, 204, 113, .7)', 'rgba(230, 126, 34, .7)', 'rgba(52, 152, 219, .7)'],
+                }],
+            
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                  'Gain/Loss',
+                  'Sale',
+                  'Remaining Value',
+                ]
+              }}
+              options = {{
+                responsive: true,
+              }}
+            />
+          </Card>
+        </Col>
       </Fragment>
     )
   }
